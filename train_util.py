526919e38bae8bff93c89e64e275bfd7a065cdcd
ayeffkay <chatton1@mail.ru>
2020-07-01 09:44:01 +0300

    Optimize output for more convenient plotting and search

--- train_util.py
+++ train_util.py
@@ -1,4 +1,3 @@
-import torch
 from copy import deepcopy
 import numpy as np
 
@@ -39,10 +38,17 @@ class TrainUtility:
         if train:
             self.train(epochs)
 
+    def gen_out(self, batch: dict, epoch_out: dict):
+        for key in epoch_out.keys():
+            try:
+              epoch_out[key] = torch.cat((epoch_out[key], batch[key]), dim=0)
+            except:
+              epoch_out[key] = batch[key]
+        return epoch_out
 
     def run_epoch(self, data, mode='valid', return_out=False):
         self.epoch_loss = 0
-        epoch_out = dict.fromkeys(['feature', 'code'])
+        epoch_out = dict.fromkeys(['feature', 'code', 'label'])
 
         with torch.set_grad_enabled(mode=='train'):
             self.model.train() if mode == 'train' else self.model.eval()
@@ -50,12 +56,8 @@ class TrainUtility:
             for i, batch in enumerate(data):
                 out = self.model(x=batch['feature'], label=batch['label'])
                 if return_out:
-                    try:
-                        epoch_out['feature'] = torch.cat((epoch_out['feature'], batch['feature']), dim=0)
-                        epoch_out['code'] = torch.cat((epoch_out['code'], out.latent_code), dim=0)
-                    except:
-                        epoch_out['feature'] = batch['feature']
-                        epoch_out['code'] = out.latent_code
+                    batch['code'] = out.latent_code
+                    epoch_out = self.gen_out(batch, epoch_out)
 
                 loss = self.criterion(out.rec, batch['loss_img'], out.mu, out.log_sigma)
                 self.epoch_loss += loss.item()
@@ -147,39 +149,34 @@ class TrainUtility:
         ax.legend()
 
 
-    def extract_latent_codes(self, dl_name='valid'):
+    def extract_out(self, dl_name='valid'):
         out = self.run_epoch(self.dataloaders[dl_name], mode='test', return_out=True)
-        feature, code = [x for key, x in out.items()]
-        return feature, code
-
-
-    def get_labels(self, dl_name='valid'):
-        labels = [batch['label'].cpu().numpy() for batch in self.dataloaders[dl_name]]
-        return np.vstack(labels)
+        return out
 
 
     def get_2d_embeddings(self, dl_name='valid', show=True):
-        self.latent_codes[dl_name] = self.extract_latent_codes(dl_name)[1]
-        labels = self.get_labels(dl_name)
+        out = self.extract_out(dl_name)
+        codes, label = out['code'].cpu().numpy(), out['label'].cpu().numpy()
 
         tsne = TSNE(n_components=2)
-        embeddings = tsne.fit_transform(self.latent_codes[dl_name].cpu().numpy())
+        embeddings = tsne.fit_transform(codes)
         if show:
-            self.plot_2d_embeddings(embeddings, labels)
-        return embeddings, labels
+            self.plot_2d_embeddings(embeddings, label)
+        return embeddings, label
 
 
     @staticmethod
     def plot_2d_embeddings(embeddings, labels):
         plt.title('t-SNE projection')
-        plt.scatter(embeddings[:, 0], embeddings[:, 1], c=labels, cmap='Set1', alpha=0.5)
+        x, y = np.hsplit(embeddings, 2)
+        plt.scatter(x, y, c=labels, cmap='Set1', alpha=0.5)
     
     
     def get_similar(self, img, n_neighbors=5, show=True):
         nn = NearestNeighbors(n_neighbors=n_neighbors)
         dl_name = 'train'
-        x, self.latent_codes[dl_name] = self.extract_latent_codes(dl_name)
-        nn.fit(self.latent_codes[dl_name].cpu().numpy())
+        out = self.extract_out(dl_name)
+        nn.fit(out['code'].cpu().numpy())
 
         if isinstance(img, np.ndarray):
             img = data_utils.np_to_tensor(img)
@@ -191,10 +188,11 @@ class TrainUtility:
             img_code = self.model.encode(img)[0].cpu().numpy()
 
         dist, ind = nn.kneighbors(img_code, n_neighbors=n_neighbors, return_distance=True)
-        dist = dist.squeeze(0)
-        ind = ind.squeeze(0)
+        dist, ind = list(map(lambda x: x.squeeze(0), [dist, ind]))
+
         if show:
             dist = list(map(lambda d: 'Dist.={:.3f}'.format(d), dist))
-            data_utils.plot_gallery(torch.cat([img, x[ind]], dim=0), n_row=1, n_col=n_neighbors + 1, 
-                         subplot_titles = ['Original image'] + dist)
-        return dist, x[ind]
+            data_utils.plot_gallery(torch.cat([img, out['feature'][ind]], dim=0), 
+                                    n_row=1, n_col=n_neighbors + 1, 
+                                    subplot_titles = ['Original image'] + dist)
+        return dist, out['feature'][ind]
